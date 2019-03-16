e2fb94c9d621698df2f5e701f8696e832ac99cd5
AoiHosizora <a970335605@hotmail.com>
2019-03-16 13:40:02 +0800

    Use Group Class

--- app/src/main/java/com/baibuti/biji/Data/Note.java
+++ app/src/main/java/com/baibuti/biji/Data/Note.java
@@ -17,8 +17,7 @@ public class Note implements Serializable {
     private String Title;
     private String Content;
 
-    private int GroupId;
-    private String GroupName;
+    private Group GroupLabel;
 
     private Date CreateTime;
     private Date UpdateTime;
@@ -30,29 +29,59 @@ public class Note implements Serializable {
         this.UpdateTime = new Date();
     }
 
+    //////////////////////////////////////////////////
 
-    public void setId(int id) { this.Id = id; }
+    public void setId(int id) {
+        this.Id = id;
+    }
 
-    public void setTitle(String Title) { this.Title = Title;}
-    public void setContent(String Content) { this.Content = Content; }
+    public void setTitle(String Title) {
+        this.Title = Title;
+    }
 
-    public void setGroupId(int GroupId) { this.GroupId = GroupId; }
-    public void setGroupName(String GroupName) { this.GroupName = GroupName; }
+    public void setContent(String Content) {
+        this.Content = Content;
+    }
 
-    public void setCreateTime(Date CreateTime) { this.CreateTime = CreateTime; }
-    public void setUpdateTime(Date UpdateTime) { this.UpdateTime = UpdateTime; }
+    public void setGroupLabel(Group GroupLabel) {
+        this.GroupLabel = GroupLabel;
+    }
 
-    public int getId() { return this.Id; }
+    public void setCreateTime(Date CreateTime) {
+        this.CreateTime = CreateTime;
+    }
 
-    public String getTitle() { return this.Title;}
-    public String getContent() { return this.Content; }
+    public void setUpdateTime(Date UpdateTime) {
+        this.UpdateTime = UpdateTime;
+    }
 
-    public int getGroupId() { return this.GroupId; }
-    public String getGroupName() { return this.GroupName; }
+    //////////////////////////////////////////////////
 
-    public Date getCreateTime() { return this.CreateTime; }
-    public Date getUpdateTime() { return this.UpdateTime; }
+    public int getId() {
+        return this.Id;
+    }
 
+    public String getTitle() {
+        return this.Title;
+    }
+
+    public String getContent() {
+        return this.Content;
+    }
+
+    public Group getGroupLabel() {
+        return this.GroupLabel;
+    }
+
+    public Date getCreateTime() {
+        return this.CreateTime;
+    }
+
+    public Date getUpdateTime() {
+        return this.UpdateTime;
+    }
+
+    //////////////////////////////////////////////////
 
     public String getUpdateTimeShortString() {
         SimpleDateFormat df;
