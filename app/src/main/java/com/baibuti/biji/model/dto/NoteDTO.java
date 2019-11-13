8bdf533619a3a943357f7a06ca689808522e45cb
AoiHosizora <a970335605@hotmail.com>
2019-11-13 10:56:28 +0800

    Change NoteDTO Interface For GroupDTO Nesting Class

--- app/src/main/java/com/baibuti/biji/model/dto/NoteDTO.java
+++ app/src/main/java/com/baibuti/biji/model/dto/NoteDTO.java
@@ -1,6 +1,5 @@
 package com.baibuti.biji.model.dto;
 
-import com.baibuti.biji.model.po.Group;
 import com.baibuti.biji.model.po.Note;
 
 import java.io.Serializable;
@@ -14,25 +13,24 @@ public class NoteDTO implements Serializable {
     private int id;
     private String title;
     private String content;
-    private int group_id;
+    private GroupDTO group; // <<<
     private Date create_time;
     private Date update_time;
 
-    private NoteDTO(int id, String title, String content, int group_id, Date create_time, Date update_time) {
+    private NoteDTO(int id, String title, String content, GroupDTO group, Date create_time, Date update_time) {
         this.id = id;
         this.title = title;
         this.content = content;
-        this.group_id = group_id;
+        this.group = group;
         this.create_time = create_time;
         this.update_time = update_time;
     }
 
     /**
-      * NoteDTO -> Note, unset-group !!!!!!
+      * NoteDTO -> Note
       */
      public Note toNote() {
-         // TODO 临时 Group
-         return new Note(id, title, content, new Group(group_id), create_time, update_time);
+         return new Note(id, title, content, group.toGroup(), create_time, update_time);
      }
 
     /**
@@ -41,14 +39,13 @@ public class NoteDTO implements Serializable {
     public static NoteDTO toNoteDTO(Note note) {
         if (note == null) return null;
         return
-            new NoteDTO(note.getId(), note.getTitle(), note.getContent(), note.getGroup().getId(), note.getCreateTime(), note.getUpdateTime());
+            new NoteDTO(note.getId(), note.getTitle(), note.getContent(), GroupDTO.toGroupDTO(note.getGroup()), note.getCreateTime(), note.getUpdateTime());
     }
 
     /**
      * NoteDTO[] -> Note[]
      */
     public static Note[] toNotes(NoteDTO[] notesDTO) {
-        // TODO 临时 Group
         if (notesDTO == null) return null;
         Note[] notes = new Note[notesDTO.length];
         for (int i = 0; i < notesDTO.length; i++)
